5841f24db278963f7a947c92c84ff0194d501137
Luana Feitosa <luanabfeitosa@outlook.com>
2023-09-14 19:31:02 -0300

    criando projeto

--- Bot mensages/main.py
+++ Bot mensages/main.py
@@ -8,6 +8,6 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
     
-
+    
 client = discord.Client(intents=discord.Intents.default())
 client.run('MTE1MTk3NjI3MDM5NDA1MjczOQ.G4Rl-Y.s7talVoW5jH8A3oFM3TPZkbfzsekfIZ9XPofkw')
